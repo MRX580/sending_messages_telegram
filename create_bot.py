d6308965d333f9497effee9b1ec4b8a7689a96d7
MRX580 <andrejchenko0511@mail.ru>
2022-09-29 10:45:06 +0200

    commit

--- create_bot.py
+++ create_bot.py
@@ -1,8 +1,5 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
 
 brain = MemoryStorage()
 bot = Bot(token='TOKEN')
